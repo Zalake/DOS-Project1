4b2689b1e0f6d4848e1bf0a98b7d845c3e45ee64
Shrishail Zalake <shrishailzalake@gmail.com>
2020-09-10 23:21:03 -0400

    Used Acctors

--- Program.fsx
+++ Program.fsx
@@ -2,28 +2,51 @@
 
 open System
 
-(*[<EntryPoint>]
-let main (args : string[]) =
-    if args.Length <> 2 then
-        failwith "Error: Expected arguments <greeting> and <thing>"
-    let last, k = args.[1], args.[2]*)
-    
+open Akka.FSharp
+open Akka.Actor
 
-//for arg in fsi.CommandLineArgs do
- //   printfn "%s" arg
+let system = System.create "system" (Configuration.defaultConfig())
 
-let mutable sqSum = 0
-let mutable sqRt = 0.0
-let start = 1 
-let last = 40   //N - hardcoded the value as of now
-let k = 24    // Hardcoded the value of K as of now
 
-for i = start to last do
+type SampleTypes= TupleType of int * int
+let mutable count=0
+//functions-------
+let squareSummation start k :bool =
+    let mutable sqSum = 0
+    let mutable sqRt = 0.0
     sqSum <- 0
-    for j = i to (i+k-1) do
-        sqSum <- sqSum + (j*j)
+    for i = start to (start+k-1) do
+        sqSum <- sqSum + (i*i)
     sqRt <- sqrt (float sqSum)
-    if sqRt - floor sqRt = 0.0 then printfn "%i" i
+
+    if sqRt - floor sqRt = 0.0 then
+        true
+    else
+        false
+(*let sss=squareSummation 9 24
+if(sss)then
+    printf "true"*)
+//Actor code block
+let lucasActor(mailbox: Actor<_>)=
+    let rec loop() = actor {
+        let! TupleType(x,y) = mailbox.Receive()
+        if(squareSummation x y)then
+            count <- count+1
+        return! loop()
+    }
+    loop()
+//intToString to create new actors
+let int2String (x: int) = string x
+let n=40 //hardcoded
+let k=24 //hardcoded
+
+for i=1 to n-k do
+    let ActorName=int2String i
+    //spawn new actor in every iteration and send the input tuple
+    spawn system ActorName lucasActor <! TupleType(i,k)
+System.Console.ReadLine() |> ignore
+printfn "final answer is %i" count
+System.Console.ReadLine() |> ignore
 
 
    

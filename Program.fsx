4366ee651258db4dfc97df97343ae849dbcd193c
Shrishail Zalake <shrishailzalake@gmail.com>
2020-09-10 23:42:08 -0400

    Printing expected numbers

--- Program.fsx
+++ Program.fsx
@@ -1,7 +1,3 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
-
 open Akka.FSharp
 open Akka.Actor
 
@@ -9,20 +5,17 @@ let system = System.create "system" (Configuration.defaultConfig())
 
 
 type SampleTypes= TupleType of int * int
-let mutable count=0
 //functions-------
-let squareSummation start k :bool =
+let squareSummation start k =
     let mutable sqSum = 0
     let mutable sqRt = 0.0
     sqSum <- 0
     for i = start to (start+k-1) do
         sqSum <- sqSum + (i*i)
     sqRt <- sqrt (float sqSum)
-
     if sqRt - floor sqRt = 0.0 then
-        true
-    else
-        false
+        printf "%i\n" start
+ 
 (*let sss=squareSummation 9 24
 if(sss)then
     printf "true"*)
@@ -30,31 +23,16 @@ if(sss)then
 let lucasActor(mailbox: Actor<_>)=
     let rec loop() = actor {
         let! TupleType(x,y) = mailbox.Receive()
-        if(squareSummation x y)then
-            count <- count+1
-        return! loop()
+        squareSummation x y
     }
     loop()
 //intToString to create new actors
 let int2String (x: int) = string x
-let n=40 //hardcoded
-let k=24 //hardcoded
+let n=30
+let k=2
 
 for i=1 to n-k do
     let ActorName=int2String i
     //spawn new actor in every iteration and send the input tuple
     spawn system ActorName lucasActor <! TupleType(i,k)
 System.Console.ReadLine() |> ignore
-printfn "final answer is %i" count
-System.Console.ReadLine() |> ignore
-
-
-   
-
-
-
-
-
-
-
-       
